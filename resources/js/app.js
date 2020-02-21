200e00bc32c84a45da1bf76467378f9a02d4fafb
mohamadirfann <mohamadirfannurfansyah@gmail.com>
2020-02-21 16:37:47 +0700

    second commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,71 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import moment from 'moment';
+import {
+    Form,
+    HasError,
+    AlertError
+} from 'vform';
+
+import swal from 'sweetalert2';
+window.swal = swal;
+
+const Toast = swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    onOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+});
+
+window.Toast = Toast;
+
+window.Form = Form;
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+import VueProgressBar from 'vue-progressbar'
+Vue.use(VueProgressBar, {
+    color: 'rgb(143, 255, 199)',
+    failedColor: 'red',
+    height: '2px'
+})
+
+
+let routes = [{
+        path: '/dashboard',
+        component: require('./components/Dashboard.vue').default
+    },
+    {
+        path: '/users',
+        component: require('./components/Users.vue').default
+    },
+    {
+        path: '/profile',
+        component: require('./components/Profile.vue').default
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes // short for `routes: routes`
+})
+
+Vue.filter('upText', function (text) {
+    return text.charAt(0).toUpperCase() + text.slice(1)
+});
+
+Vue.filter('myDate', function (created) {
+    return moment(created).format('MMMM Do YYYY');
+});
 
 /**
  * The following block of code may be used to automatically register your
@@ -29,4 +94,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
 });
